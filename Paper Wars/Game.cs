60283b54261125e8e2ef55cbdfec0a8b506038cb
Reginald Thompson <reginaldjthompsonjr23@gmail.com>
2023-09-18 15:47:20 -0500

    Added game class

---
+++ Paper Wars/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paper_Wars
+{
+    class Game
+    {
+        public void Run()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
---
+++ Paper Wars/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Paper_Wars;
+
+namespace PaperWars
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Game game = new Game();
+            game.Run();
+        }
+    }
+}
